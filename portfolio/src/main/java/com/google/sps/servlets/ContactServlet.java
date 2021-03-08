14b4e748120af1071c7433e613e9d583714f6332
Oswaldo Escobedo <nightmime14@gmail.com>
2021-03-08 07:22:56 +0000

    Get info from form, print values to server logs, and write value

--- portfolio/src/main/java/com/google/sps/servlets/ContactServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ContactServlet.java
@@ -12,13 +12,22 @@ public class ContactServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+    // Get the values entered in the form.
+    String fullNameValue = request.getParameter("fullname");
+    String emailValue = request.getParameter("email");
+    String subjectValue = request.getParameter("subject");
+    String messageValue = request.getParameter("message");
 
     // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    System.out.println("You submitted: " + fullNameValue);
+    System.out.println("You submitted: " + emailValue);
+    System.out.println("You submitted: " + subjectValue);
+    System.out.println("You submitted: " + messageValue);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    response.getWriter().println("You submitted: " + fullNameValue);
+    response.getWriter().println("You submitted: " + emailValue);
+    response.getWriter().println("You submitted: " + subjectValue);
+    response.getWriter().println("You submitted: " + messageValue);
   }
 }
